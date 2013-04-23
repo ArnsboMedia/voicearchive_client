623cca79d22be1a97e7fa0ee55f387d15a6feae7
Claus Witt <claus@wittnezz.dk>
2013-04-23 18:42:56 +0200

    added spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'webmock/rspec'
+require 'vcr'
+
+require 'simplecov'
+SimpleCov.start
+
+
+RSpec.configure do |c|
+  c.include WebMock::API
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/cassette_library'
+  c.hook_into :webmock
+  c.ignore_localhost = true
+  c.default_cassette_options = { :record => :none }
+end
