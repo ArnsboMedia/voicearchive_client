57f79e5206d22f1bb1234fc9288f2359138d8e61
Claus Witt <claus@wittnezz.dk>
2013-06-26 14:51:24 +0200

    factory for getting clients

---
+++ lib/voicearchive/factory.rb
@@ -0,0 +1,17 @@
+require 'activesupport/lib/active_support/inflector/inflections'
+module Voicearchive
+  class Factory
+    attr_reader :api_key
+    def initialize api_key
+      @api_key = api_key
+    end
+
+    def get_client name
+      name = "#{name}_client"
+      require "voicearchive/#{name}"
+      constant_name = "Voicearchive::#{name.camelize}"
+      constant_name.constantize.new(api_key)
+    end
+  end
+end
+
