616136bae55d553703f6ec7d46953b769b4e3334
Claus Witt <claus@wittnezz.dk>
2013-06-26 11:12:10 +0200

    added order client

---
+++ lib/voicearchive/order_client.rb
@@ -0,0 +1,26 @@
+require 'voicearchive/client'
+module Voicearchive
+  class OrderClient < Client
+    def get_order(order_id)
+      response = call("orders/#{order_id}")
+      JSON.parse(response.body)
+    end
+
+    def task_client
+      @task_client ||= TaskClient.new(@api_key)
+      @task_client
+    end
+
+    def task_with_order(task_id, customer_id=false)
+      task = task_client.get_task(task_id)
+      order = get_order(task['order_id'])
+      if customer_id
+        if order['customer'].to_i != customer_id.to_i
+          raise 'Customer does not match'
+        end
+      end
+      task['order'] = order
+      task
+    end
+  end
+end
