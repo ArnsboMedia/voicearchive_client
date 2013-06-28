c6d2e26e716db8b7df2d485c0145f7b5ee7fa2d1
Kåre Sørensen <kaare@voicearchive.com>
2013-06-28 13:52:06 +0200

    now possible to get all virtual recordings

--- lib/voicearchive/virtualrecording_client.rb
+++ lib/voicearchive/virtualrecording_client.rb
@@ -5,5 +5,9 @@ module Voicearchive
       response = call("virtualrecording/#{id}", {:includeRelations=>1})
       JSON.parse(response.body)
     end
+    def get_virtual_recordings
+      response = call("virtualrecording", {:includeRelations=>1})
+      JSON.parse(response.body)
+    end
   end
 end
