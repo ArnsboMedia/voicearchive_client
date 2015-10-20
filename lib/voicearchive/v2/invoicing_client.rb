51cd38532d0758e56f1620ee259f13f69d4cbcd7
Mathias Jakobsen <mathiasjakobsen@me.com>
2015-10-20 11:09:23 +0200

    Seperates v2 enpoints to a new module, so it wont screw with existing setups.

---
+++ lib/voicearchive/v2/invoicing_client.rb
@@ -0,0 +1,16 @@
+require 'voicearchive/client'
+module Voicearchive
+  module V2
+    class InvoicingClient < Client
+      def get_orders_for_invoicing
+        response = call('invoicing')
+        JSON.parse(response.body)
+      end
+
+      def mark_orders_invoiced orders
+        response = call('invoicing', orders, 'post')
+        JSON.parse(response.body)
+      end
+    end
+  end
+end
