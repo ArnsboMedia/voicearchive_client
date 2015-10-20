e430d980ac82a3a40ec13a1a13256ce98bb2c1eb
Mathias Jakobsen <mathiasjakobsen@me.com>
2015-10-20 11:06:24 +0200

    Adds invoicing read end-point

--- lib/voicearchive/invoicing_client.rb
+++ lib/voicearchive/invoicing_client.rb
@@ -1,8 +1,13 @@
 require 'voicearchive/client'
 module Voicearchive
   class InvoicingClient < Client
-    def get
+    def get_orders_for_invoicing
       response = call('invoicing')
+      JSON.parse(response.body)
+    end
+
+    def mark_orders_invoiced orders
+      response = call('invoicing', orders, 'post')
       JSON.parse(response.body)
     end
   end
