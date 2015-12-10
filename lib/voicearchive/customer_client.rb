1f9959305369682ff9d36ca86d46462a1f0a923f
Mathias Jakobsen <mathiasjakobsen@me.com>
2015-12-10 08:54:57 +0100

    Adds methods for getting customer contacts.

--- lib/voicearchive/customer_client.rb
+++ lib/voicearchive/customer_client.rb
@@ -11,5 +11,15 @@ module Voicearchive
       response = call("customer/#{id}", params)
       JSON.parse(response.body)
     end
+    def get_customer_contacts(customerId, params={})
+      params = set_simple_endpoint_default_values(params)
+      response = call("customer/#{customerId}/contacts", params)
+      JSON.parse(response.body)
+    end
+    def get_customer_contact(id, params={})
+      params = set_simple_endpoint_default_values(params)
+      response = call("customercontact/#{id}", params)
+      JSON.parse(response.body)
+    end
   end
 end
