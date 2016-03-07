f5fcf49afb2041ccf3ec089fe264826aefc9e37f
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-03-07 21:13:06 +0100

    Better subtask name extraction.

--- lib/voicearchive/v2/supplier_client.rb
+++ lib/voicearchive/v2/supplier_client.rb
@@ -13,8 +13,9 @@ module Voicearchive
       end
 
       def method_missing(method_sym, *arguments, &block)
-        if method_sym.to_s =~ /^get_supplier_(.*)$/ && subtasks.include?(method_sym)
-          response = call("supplier/#{arguments.first}/#{$1}")
+        subtask = method_sym.to_s.split('get_supplier_')[-1]
+        if subtask.include?(subtask)
+          response = call("supplier/#{arguments.first}/#{subtask}")
           JSON.parse(response.body)
         else
           super
