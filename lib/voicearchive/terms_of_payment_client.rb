1e864805bd0c222c0e3d133bdfac601689eb4bbb
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-07-13 14:13:47 +0200

    Adds PUT to terms_of_payment

--- lib/voicearchive/terms_of_payment_client.rb
+++ lib/voicearchive/terms_of_payment_client.rb
@@ -6,5 +6,10 @@ module Voicearchive
       response = call('TermOfPayment')
       JSON.parse(response.body)
     end
+
+    def update_terms_of_payment(id, params = {})
+      response = call("TermOfPayment/#{id}", params, 'put')
+      JSON.parse(response)
+    end
   end
 end
