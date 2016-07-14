757ba560981b906539983702ef8a5498a742a64a
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-07-14 15:54:04 +0200

    adds common client, for arbitraty use

---
+++ lib/voicearchive/common_client.rb
@@ -0,0 +1,28 @@
+require 'voicearchive/client'
+
+module Voicearchive
+  class CommonClient < Client
+    attr_accessor :endpoint
+
+    def all(params = {})
+      params = set_simple_endpoint_default_values(params)
+      response = call("#{@endpoint}", params, 'get')
+      JSON.parse(response.body)
+    end
+
+    def create(params = {})
+      response = call(@endpoint, params, 'post')
+      JSON.parse(response.body)
+    end
+
+    def update(id, params = {})
+      response = call("#{@endpoint}/#{id}", params, 'put')
+      JSON.parse(response.body)
+    end
+
+    def destroy(id)
+      response = call("#{@endpoint}/#{id}", params, 'delete')
+      JSON.parse(response.body)
+    end
+  end
+end
