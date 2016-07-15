da12b96bad46120531223338fdd830b4eacb551e
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-07-15 10:12:06 +0200

    Adds .find til Common

--- lib/voicearchive/common_client.rb
+++ lib/voicearchive/common_client.rb
@@ -10,6 +10,12 @@ module Voicearchive
       JSON.parse(response.body)
     end
 
+    def find(id, params = {})
+      params = set_simple_endpoint_default_values(params)
+      response = call("#{@endpoint}/#{id}", params, 'get')
+      JSON.parse(response.body)
+    end
+
     def create(params = {})
       response = call(@endpoint, params, 'post')
       JSON.parse(response.body)
