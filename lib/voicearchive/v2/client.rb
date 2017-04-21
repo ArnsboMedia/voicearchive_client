a3a0dcb779db26f5f3a8e06b2c3ae94db54c0a27
Mathias Jakobsen <mathiasjakobsen@me.com>
2017-04-21 09:52:32 +0200

    Allow setting host, when initializing client

--- lib/voicearchive/v2/client.rb
+++ lib/voicearchive/v2/client.rb
@@ -3,8 +3,8 @@ require 'voicearchive/client'
 module Voicearchive
   module V2
     class Client < ::Voicearchive::Client
-      def initialize api_key='', version=2
-        super api_key, version
+      def initialize(api_key = '', version = 2, host)
+        super(api_key, version, host)
       end
     end
   end
