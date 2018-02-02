fb2e50b6c608e1d355ce6ce33b95834c423765e8
Mads Lundholm <mads@madslundholm.dk>
2018-02-02 10:01:26 +0100

    Add method to create a retake on the given task ID

--- lib/voicearchive/task_client.rb
+++ lib/voicearchive/task_client.rb
@@ -5,10 +5,20 @@ module Voicearchive
       response = call("task/#{task_id}/SetSupplier", {:supplierId=>supplier_id}, 'put')
       JSON.parse(response.body)
     end
+
     def set_task_data(task_id, supplier_id, comments, script)
       response = call("task/#{task_id}/SetTaskData", {:supplierId=>supplier_id, :comments=>comments, :script=>script}, 'put')
       JSON.parse(response.body)
     end
+
+    def create_retake(task_id, reason_id)
+      response = call("task/#{task_id}/create-retake", {
+        reasonId: reason_id
+      }, 'put');
+
+      JSON.parse(response.body)
+    end
+
     def get_task(task_id)
       response = call("ordertask/#{task_id}")
       JSON.parse(response.body)
